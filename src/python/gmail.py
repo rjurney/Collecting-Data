3ef9c1d7a2c616bda7d9c195dfce7ef0524d532e
Russell Jurney <russell.jurney@gmail.com>
2012-03-14 18:21:54 -0700

    have emails parsing into avroizable array lists, now need to get dates to ISO format

--- src/python/gmail.py
+++ src/python/gmail.py
@@ -28,17 +28,41 @@ def init_avro(output_dir, part_id, schema_path):
   )
   return df_writer
 
+def fetch_email(imap, id):
+  status, data = imap.fetch(id, '(RFC822)')
+  raw_email = data[0][1]
+  msg = email.message_from_string(raw_email)
+  text_parts = walk_msg(msg)
+  avro_parts = process_email(msg)
+  return avro_parts
+
+def parse_addrs(addr_string):
+  if addr_string:
+    ads = email.utils.getaddresses([addr_string])
+    final = []
+    for a in ads:
+      final.append(a[1])
+    address = final
+  else:
+    address = addr_string
+  return address
+
+def process_email(msg):  
+  avro_parts = {
+    'message_id': parse_addrs(msg['Message-ID']),
+    'from': parse_addrs(msg['From']),
+    'to': parse_addrs(msg['To']),
+    'cc': parse_addrs(msg['Cc']),
+    'bcc': parse_addrs(msg['Bcc']),
+    'reply_to': parse_addrs(msg['Reply-To']),
+    'in_reply_to': parse_addrs(msg['In-Reply-To']),
+    'subject': parse_addrs(msg['Subject']),
+    'date': parse_addrs(msg['Date'])
+  }
+  return avro_parts
+
 def write_email(writer, email):
   writer.append(email)
-
-def get_first_text_part(msg):
-  maintype = msg.get_content_maintype()
-  if maintype == 'multipart':
-    for part in msg.get_payload():
-      if part.get_content_maintype() == 'text':
-        return part.get_payload()
-  elif maintype == 'text':
-    return msg.get_payload()
 
 def walk_msg(msg):
   for part in msg.walk():
@@ -46,49 +70,28 @@ def walk_msg(msg):
       continue
     yield part.get_payload(decode=1)
 
-# # MAIN
-# if (len(sys.argv) < 4):
-#   print """Usage: gmail.py <username@gmail.com> <password> <output_directory>"""
-#   exit(0)
-#
-# user_name = sys.argv[1]
-# password = sys.argv[2]
-# directory = init_directory(sys.argv[3])
+# MAIN
+if (len(sys.argv) < 4):
+  print """Usage: gmail.py <username@gmail.com> <password> <output_directory>"""
+  exit(0)
+
+user_name = sys.argv[1]
+password = sys.argv[2]
+directory = init_directory(sys.argv[3])
 
 pp = pprint.PrettyPrinter(indent=4)
 
-user_name = 'russell.jurney@gmail.com'
-password = 'K4mikazi!'
 schema_path = '/me/Collecting-Data/src/avro/email.schema'
-output_dir = init_directory('/tmp/python')
 avro_writer = init_avro(output_dir, 1, schema_path)
 imap_folder = '[Gmail]/All Mail'
 
 imap = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 imap.login(user_name,password)
 status, count = imap.select(imap_folder)
-status, data = imap.fetch(count[0], '(RFC822)')
-raw_email = data[0][1]
-email = email.message_from_string(raw_email)
-text_parts = walk_msg(email)
 
-avro_parts = {
-  'message_id': email['Message-ID'],
-  'from': email['From'],
-  'to': email['To'],
-  'cc': email['Cc'],
-  'bcc': email['Bcc'],
-  'reply_to': email['Reply-To'],
-  'in_reply_to': email['In-Reply-To'],
-  'subject': email['Subject'],
-  'date': email['Date']
-}
-
-writer.append({'message_id': '4fwegaeg@daofma.com-7',
-               'from': ['russell@gmail.com'],
-               'to': ['bob@bob.com'],
-               'subject': 'Testing python!',
-               'date': '2001'})
+email_hash = fetch_email(imap, '5')
+avro_writer.append(email_hash)
+avro_writer.close()
 
 imap.close()
 imap.logout()
